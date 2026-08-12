ea0e4018716cd302a5d8af1379b71b7976084f99
Happy-Birthday9 <websiterun904@gmail.com>
2026-08-12 11:22:11 +0600

    Create config.py

---
+++ config.py
@@ -0,0 +1,12 @@
+# config.py
+
+# Telegram BotFather থেকে পাওয়া Bot Token
+BOT_TOKEN = "8661744403:AAFpn6awjmN6BG68qXrub85siQvUBuJ6LT4"
+
+# Admin Telegram User ID
+# একাধিক admin হলে এভাবে লিখবে:
+# ADMIN_IDS = [8136997138, 8136997138]
+
+ADMIN_IDS = [
+    8136997138
+]
