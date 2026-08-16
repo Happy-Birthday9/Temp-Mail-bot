daab190c9f5dfcfbc56a1a3c05b53978c3a59e41
Happy-Birthday9 <websiterun904@gmail.com>
2026-08-16 15:59:55 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -2840,10 +2840,15 @@ application.add_handler(
     MessageHandler(filters.TEXT & ~filters.COMMAND, text_handler)
 )
 
+# Text
+application.add_handler(
+    MessageHandler(filters.TEXT & ~filters.COMMAND, text_handler)
+)
+
 # Error
 application.add_error_handler(error_handler)
 
-    print("🤖 Temp Mail Bot is running...")
+print("🤖 Temp Mail Bot is running...")
     print(f"📩 Auto inbox: every {POLL_SECONDS}s")
 
     application.run_polling(drop_pending_updates=True)
